8dbb3f7c0459f0179ddf72193a71fd031a3b5470
Rakibul-Islam-GitHub <freelancing.everyday@gmail.com>
2021-11-06 11:10:36 +0600

    new task will be added in the list

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [enteredTask, setEnteredTask]= useState('')
+  const [task, setTask]= useState([])
+  const handleTask=(enteredTask)=>{
+    
+    const result=  [...task, enteredTask ]
+     setTask(result)
+    
+    
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container}> 
+      
+      <View style={styles.inputContainer}>
+      <TextInput onChangeText={(value)=>setEnteredTask(value)} style={{width:'80%', borderWidth:1, borderColor:'gray', padding:5}} autoCapitalized="words"   placeholder="Enter Task"></TextInput>
+      <Button onPress={()=>handleTask(enteredTask)} title="Add"></Button>
+      </View>
+
+      <View >
+        {task.map((task)=>(
+          <View key={task} style={styles.taskListContainer}><Text style={{color:'white', fontWeight:'bold',}} >{task}</Text></View>
+        )
+        )}
+      </View>
+      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{
+    padding:60,
   },
+  inputContainer:{
+    flexDirection:'row',
+    justifyContent:'space-between',
+  },
+  taskListContainer:{
+    backgroundColor:'chocolate',
+    padding:15,
+    marginVertical:6,
+    
+    
+    
+  }
 });
