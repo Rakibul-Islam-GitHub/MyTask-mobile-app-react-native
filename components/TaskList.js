8c6f0fc58a0ad71d2026d91837a1e912c3499c1b
Rakibul-Islam-GitHub <freelancing.everyday@gmail.com>
2021-11-07 14:25:40 +0600

    task view list styles added

--- components/TaskList.js
+++ components/TaskList.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import {Text, View, StyleSheet} from 'react-native'
+import { MaterialIcons } from "@expo/vector-icons";
 
 const TaskList = (props) => {
     return (
-        <View key={props.item} style={styles.taskListContainer}><Text style={{color:'white', fontWeight:'bold',}} >{props.item}</Text></View>
+      <View style={styles.itemWrapper}>
+          <View key={props.item} style={styles.taskListContainer}>
+           <MaterialIcons
+              style={{ marginRight: 5, color: "#78e08f" }}
+              name='visibility'
+              size={22}
+              
+            />
+        <Text style={{ color: "white", fontWeight: "bold", textTransform:'capitalize' }}>{props.item}</Text>
+
+        
+      </View>
+      <MaterialIcons
+        
+        name='delete'
+        size={25}
+        color='#eb2f06'
+      />
+      </View>
     );
 
 
@@ -11,11 +30,22 @@ const TaskList = (props) => {
 };
 
 const styles = StyleSheet.create({
+    itemWrapper:{
+    backgroundColor:'#0a3d62',
+    padding: 15,
+    borderRadius: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 15,
+    },
     taskListContainer:{
-        backgroundColor:'chocolate',
-        padding:15,
-        marginVertical:6,
+        flexDirection: "row",
+        justifyContent: "center",
+        
+
       },
+    
       
 })
 
